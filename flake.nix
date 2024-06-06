47516ad9277d41f5f52baf828e77a962282a3fa0
Purple Clay <purpleclaygh@gmail.com>
2024-06-06 17:06:37 +0100

    chore: switch to using renovate for managing dependencies (#63)

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Easily import a GPG key within any CI workflow";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
