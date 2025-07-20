bce2785eb97ce94d6a02e3e6b638d33fc5454bd8
purpleclay <purpleclaygh@gmail.com>
2025-07-20 08:06:21 +0100

    chore(nix): support the running of gpg-import directly through nix

--- flake.nix
+++ flake.nix
@@ -12,35 +12,53 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
 
         inherit (nixpkgs) lib;
 
+        rustToolchain = pkgs.rust-bin.stable."1.87.0".default.override {
+          extensions = ["rust-src" "cargo" "rustc"];
+        };
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+
         buildInputs = with pkgs; [
+          alejandra
+          nil
           openssl
           zlib
           libfaketime
         ];
 
-        nativeBuildInputs = with pkgs; [
-          (rust-bin.stable.latest.default.override{
-            extensions = ["rust-src" "cargo" "rustc"];
-          })
-          pkg-config
-        ]
-        ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+        nativeBuildInputs = with pkgs;
+          [
+            rustToolchain
+            pkg-config
+          ]
+          ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
       in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          inherit buildInputs nativeBuildInputs;
-        };
-      }
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+          };
+
+          packages.default = pkgs.callPackage ./default.nix {
+            inherit rustPlatform;
+          };
+        }
     );
 }
