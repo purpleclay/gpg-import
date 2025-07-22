b425544aa1a771d63aef8d319f7c7875366ed07c
purpleclay <purpleclaygh@gmail.com>
2025-07-22 05:36:40 +0100

    try and get static cross compilation to work

--- flake.nix
+++ flake.nix
@@ -66,6 +66,18 @@
         with pkgs; {
           devShells.default = mkShell {
             inherit buildInputs nativeBuildInputs;
+
+            # Environment variables for cargo-zigbuild cross-compilation
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.zig}/bin/zig";
+            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.zig}/bin/zig";
+            CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER = "${pkgs.zig}/bin/zig";
+            CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER = "${pkgs.zig}/bin/zig";
+
+            # Use vendor mode for libgit2 to avoid cross-compilation issues
+            LIBGIT2_SYS_USE_PKG_CONFIG = "0";
+
+            # Static linking for musl targets
+            RUSTFLAGS = "-C target-feature=+crt-static";
           };
 
           packages.default = pkgs.callPackage ./default.nix {
